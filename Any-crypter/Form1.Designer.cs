1ac8f147282aead44a56f8a696b418563990fec5
TheRavingViking <sjoerdvangalen93@gmail.com>
2017-02-09 11:11:23 +0100

    init commit

---
+++ Any-crypter/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Any_crypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 122);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(28, 66);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1392, 36);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Image = global::Any_crypter.Properties.Resources.encrypt;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Encrypt it";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Image = global::Any_crypter.Properties.Resources.encrypt;
+            this.label2.Location = new System.Drawing.Point(24, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Decrypt it";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.Location = new System.Drawing.Point(28, 214);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1392, 36);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 270);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Go!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox3.Location = new System.Drawing.Point(23, 329);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox3.Size = new System.Drawing.Size(1392, 350);
+            this.textBox3.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Any_crypter.Properties.Resources.encrypt;
+            this.pictureBox1.Location = new System.Drawing.Point(1, -6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1443, 700);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1437, 694);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Any-crypter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Any-crypter/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Any_crypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // TODO: List
+        /*
+         *      
+         *      Code netter maken
+         *      Comments toevoegen
+         *      console comments verwijderen
+         *      muziekjes updaten
+         *      rickroll toevoegen
+         *      max char toevoegen
+         *      opmaak
+         *      geluiden toevoegen aan de dev branch.
+         *      als applicatie compilen en testen.
+         *      testen
+         * 
+         */
+
+          
+
+
+        //Encrypt the input from the first input field
+        private void encrypt()
+        {
+            
+            Console.WriteLine(textBox1.Text.ToCharArray());                                             //TODO: moet nog weg.
+
+            // Set variables
+            string s = textBox1.Text;
+            int teller = 1;
+            int code = 0;
+            string ready_To_Hexa = "";
+            string hexValue = "";
+            string trimmed_String = "";
+
+            // clear text so we can set the new encrypted code
+            textBox3.Clear();
+
+            // cycle through the chars from the input
+            foreach (char c in s)
+            {
+                // here we sprankle our magic dust and create the code to encrypt.
+                Console.WriteLine((int)c);                                                              //TODO: moet nog weg.
+                teller += 1;
+                Console.WriteLine(teller);                                                              //TODO: moet nog weg.
+                code = (c + teller) * 3;
+                Console.WriteLine(code);                                                                //TODO: moet nog weg.
+
+                // hexa code maken.
+                // hele string converten naar hexa.
+                // output hexa in textbox3   
+                             
+                ready_To_Hexa+= code.ToString();
+
+                hexValue = code.ToString("X");
+                Console.WriteLine(hexValue);                                                            //TODO: moet nog weg.
+                trimmed_String += hexValue + "-".ToString();
+
+                textBox3.Text = trimmed_String.Substring(0,trimmed_String.Length -1);
+            }
+
+        }
+
+        //Decrypt
+        private void decrypt()
+        {
+            Console.WriteLine(textBox2.Text.ToCharArray());                                             //TODO: moet nog weg.
+            // Set variables
+            int parsedstring = 0;
+            int teller = 1;
+            int asciiChar = 0;
+            string decryptedText = "";
+            string s = textBox2.Text;
+            string[] split = s.Split("-".ToCharArray());
+
+            // clear text so we can set the new encrypted code
+            textBox3.Clear();
+
+
+
+            // here we sprankle our magic dust and create the words from the encryption.
+            foreach (string hexValue in split)
+            {
+                // terug van hexa code
+                // hexa naar deciamal
+                // splitten na elke 3 char's 
+                // de foreach loop in.
+
+                try
+                {
+                    parsedstring = int.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+                    Console.WriteLine(hexValue);                                                            //TODO: moet nog weg.
+                    Console.WriteLine(parsedstring);                                                        //TODO: moet nog weg.
+
+                    teller += 1;
+                    Console.WriteLine(teller);                                                              //TODO: moet nog weg.
+                    asciiChar = (parsedstring / 3) - teller;
+                    Console.WriteLine(asciiChar);                                                           //TODO: moet nog weg.
+                    decryptedText = (Convert.ToChar(asciiChar)).ToString();
+                    Console.WriteLine(decryptedText);                                                       //TODO: moet nog weg.
+                    textBox3.Text += decryptedText;
+                }
+                catch (Exception)
+                {
+                    Console.Write("error");
+                    //throw;
+                    textBox3.Text = "No valid code";
+                }
+                
+
+            }
+           
+        }
+
+        //Textbox 1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "karim")
+            {
+                karimRoll();
+            }
+            else
+            {
+                encrypt();
+                startupSound();
+            }
+        }
+
+        // Keystrokes for textbox 1
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                encrypt();
+            }
+
+        }
+
+        //Textbox 2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decrypt();
+            shutdownSound();
+        }
+
+        // Keystrokes for textbox 2
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                decrypt();
+            }
+
+        }
+        private void startupSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"startup.wav");
+            simpleSound.Play();
+
+        }
+        private void shutdownSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"shutdown.wav");
+            simpleSound.Play();
+
+        }
+
+        private void trollSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"troll.wav");
+            simpleSound.PlayLooping();
+
+        }
+
+        private void karimRoll()
+        {
+            trollSound();
+            textBox3.Text = "We're no strangers to love" + Environment.NewLine +
+"You know the rules and so do I" + Environment.NewLine +
+"A full commitment's what I'm thinking of" + Environment.NewLine +
+"You wouldn't get this from any other guy" + Environment.NewLine + Environment.NewLine +
+
+"I just want to tell you how I'm feeling" + Environment.NewLine +
+"Gotta make you understand" + Environment.NewLine + Environment.NewLine +
+
+"Never gonna give you up, never gonna let you down" + Environment.NewLine +
+"Never gonna run around and desert you" + Environment.NewLine +
+"Never gonna make you cry, never gonna say goodbye" + Environment.NewLine +
+"Never gonna tell a lie and hurt you" + Environment.NewLine + Environment.NewLine +
+
+"We've known each other for so long" + Environment.NewLine +
+"Your heart's been aching but you're too shy to say it" + Environment.NewLine +
+"Inside we both know what's been going on" + Environment.NewLine +
+"We know the game and we're gonna play it" + Environment.NewLine + Environment.NewLine +
+
+"And if you ask me how I'm feeling" + Environment.NewLine +
+"Don't tell me you're too blind to see" + Environment.NewLine + Environment.NewLine +
+
+"Never gonna give you up, never gonna let you down" + Environment.NewLine +
+"Never gonna run around and desert you" + Environment.NewLine +
+"Never gonna make you cry, never gonna say goodbye" + Environment.NewLine +
+"Never gonna tell a lie and hurt you" + Environment.NewLine + Environment.NewLine +
+
+"Never gonna give you up, never gonna let you down" + Environment.NewLine +
+"Never gonna run around and desert you" + Environment.NewLine +
+"Never gonna make you cry, never gonna say goodbye" + Environment.NewLine +
+"Never gonna tell a lie and hurt you" + Environment.NewLine + Environment.NewLine +
+
+"We've known each other for so long" + Environment.NewLine +
+"Your heart's been aching but you're too shy to say it" + Environment.NewLine +
+"Inside we both know what's been going on" + Environment.NewLine +
+"We know the game and we're gonna play it" + Environment.NewLine + Environment.NewLine +
+
+"I just want to tell you how I'm feeling" + Environment.NewLine +
+"Gotta make you understand" + Environment.NewLine + Environment.NewLine +
+
+"Never gonna give you up, never gonna let you down" + Environment.NewLine +
+"Never gonna run around and desert you" + Environment.NewLine +
+"Never gonna make you cry, never gonna say goodbye" + Environment.NewLine +
+"Never gonna tell a lie and hurt you";
+        }
+
+    }
+}
